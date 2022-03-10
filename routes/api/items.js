2e0b0839946d5749ae453def9cfaf5264f19c35a
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-03-10 22:16:32 +0200

    I hope work

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,41 +1,39 @@
-  const express = require('express')
-  const router = express.Router();
-  const auth = require('../../midlewares/auth')
-  // Items Model
+const express = require("express");
+const router = express.Router();
+const auth = require("../../midlewares/auth");
+// Items Model
 
-  const Item = require('../../models/item')
+const Item = require("../../models/item");
 
 // @route GET api/items
 // @route Get all items
 // @access Public
 
-router.get('/',(req,res) =>{
-    Item.find()
-        .sort({date:-1 })
-        .then(items => res.json(items))
-})
-
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+});
 
 // @route POST api/items
 // @desc  Create A Post
 // @access Private
 
-router.post('/',auth,(req,res) =>{
+router.post("/", auth, (req, res) => {
   const newItem = new Item({
-      name:req.body.name 
-  })
-  newItem.save().then(item => res.json(item))
-})
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item.name));
+});
 
-
-// @route POST api/items
+// @route Delete Item
 // @desc  Delete Post
 // @access Private
 
-router.delete('/:id',auth,(req,res) => {
-    Item.findById(req.params.id)
-        .then(item => item.remove().then( () => res.json({success:true})))
-        .catch(err => res.status(404).json({success:false}))
+router.delete("/:id", auth, (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
-module.exports =  router
+module.exports = router;
